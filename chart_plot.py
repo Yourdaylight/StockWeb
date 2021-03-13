78b8639713f9e516fedd6c775894c88afbfa699d
lzh <52725224+Yourdaylight@users.noreply.github.com>
2021-03-14 01:06:35 +0800

    优化构图函数参数

--- chart_plot.py
+++ chart_plot.py
@@ -1,95 +1,101 @@
-
 from datetime import datetime
-import tushare as ts
+
 import pandas as pd
 import plotly as py
 import plotly.graph_objs as go
+import tushare as ts
+import traceback
+
 pyplt = py.offline.plot
-#读取股票代码与名称字典
+# 读取股票代码与名称字典
 ts.set_token('b94ecd1e37a3628890e89b90e0259b6db72b0ae0b17dadd2c28c5c4c')
 pro = ts.pro_api()
 
-df=pd.read_csv("stock_code.csv")
+df = pd.read_csv("stock_code.csv")
+
+
 # df = pro.daily(ts_code='000001.sz', start_date='20190701', end_date='20190930')#直接保存
 
 class Chart_Plot:
-    global pro,df
-    def __init__(self,start_date,end_date,stock1="000001",stock2="000002"):
-        #处理参数格式，符合tushare调用规范
-        self.start_date=start_date.replace("-","")
-        self.end_date=end_date.replace("-","")
-        #获取指定股票代码的名称
-        temp=df.loc[df.ts_code.str.contains(stock1)]
-        self.stock1_code=temp["ts_code"].values[0]
-        self.stock1=pro.daily(ts_code=self.stock1_code, start_date=self.start_date, end_date=self.end_date)
-        self.name1=temp['name'].values[0]
-        #获取要对比的股票代码的名称
-        temp = df.loc[df.ts_code.str.contains(stock2)]
-        self.stock2_code = temp["ts_code"].values[0]
-        self.stock2=pro.daily(ts_code=self.stock2_code, start_date=self.start_date, end_date=self.end_date)
-        self.name2=temp['name'].values[0]
+    global pro, df
 
+    def __init__(self, **params):
 
-    def candle_stick(self,period="day"):#日K线图
-        #默认获取日k,如果period不为日k，则获取对应值
-        if period=="周k":
-            self.stock1=pro.weekly(ts_code=self.stock1_code,start_date=self.start_date,end_date=self.end_date)
-        elif period=="月k":
-            self.stock1 = pro.monthly(ts_code=self.stock1_code, start_date=self.start_date, end_date=self.end_date)
+        # 处理参数格式，符合tushare调用规范
+        # import pdb
+        # pdb.set_trace()
+        self.start_date = params.get("start_date").replace("-", "")
+        self.end_date = params.get("end_date").replace("-", "")
+        # 获取指定股票代码的名称
+        self.stock1_code = params.get("stock_id", "000001")
 
+        temp = df.loc[df.ts_code.str.contains(self.stock1_code)]
+        self.stock1_code = list(temp["ts_code"])[0]
+        self.stock1 = pro.daily(ts_code=self.stock1_code, start_date=self.start_date, end_date=self.end_date)
+        self.name1 = temp['name'].values[0]
+        # 获取要对比的股票代码的名称
+        self.stock2_code = params.get("stock_id2", "000002")
+        temp = df.loc[df.ts_code.str.contains(self.stock2_code)]
+        self.stock2_code = list(temp["ts_code"])[0]
+        self.stock2 = pro.daily(ts_code=self.stock2_code, start_date=self.start_date, end_date=self.end_date)
+        self.name2 = temp['name'].values[0]
 
+        self.index_type = params.get("index_type")
 
-        strdate =self.stock1['trade_date'].tolist()
-        #日期字符串转时间序列
-        date=[]
+    def candle_stick(self, period="day"):  # 日K线图
+        # 默认获取日k,如果period不为日k，则获取对应值
+        try:
+            if period == "周k":
+                self.stock1 = pro.weekly(ts_code=self.stock1_code, start_date=self.start_date, end_date=self.end_date)
+            elif period == "月k":
+                self.stock1 = pro.monthly(ts_code=self.stock1_code, start_date=self.start_date, end_date=self.end_date)
+        except Exception as e:
+            traceback.print_exc()
+            return "该股票数据不存在"
+        strdate = self.stock1['trade_date'].tolist()
+        # 日期字符串转时间序列
+        date = []
         for i in strdate:
-            X = datetime.strptime(i, '%Y%m%d')           
-            date.append(X)         
-        candle_trace = go.Candlestick(x = date,
-                                      open = self.stock1.open,
-                                      high = self.stock1.high,
-                                      low = self.stock1.low,
-                                      close = self.stock1.close,
-                                      increasing=dict(line=dict(color= '#ff0000')),
-                                      decreasing=dict(line=dict(color= '#00ff00')),
-                                      name = self.name1)
+            X = datetime.strptime(i, '%Y%m%d')
+            date.append(X)
+        candle_trace = go.Candlestick(x=date,
+                                      open=self.stock1.open,
+                                      high=self.stock1.high,
+                                      low=self.stock1.low,
+                                      close=self.stock1.close,
+                                      increasing=dict(line=dict(color='#ff0000')),
+                                      decreasing=dict(line=dict(color='#00ff00')),
+                                      name=self.name1)
         candle_data = [candle_trace]
-        candle_layout = {'title': self.name1,'yaxis': {'title': '价格'}}
+        candle_layout = {'title': self.name1, 'yaxis': {'title': '价格'}}
         candle_fig = dict(data=candle_data, layout=candle_layout)
         div = pyplt(candle_fig, output_type='div', include_plotlyjs=False, auto_open=False, show_link=False)
         return div
-    
-    
-    def twoline_graph(self): #半年线图
-        #self.stock1['close']取出来的值都是带有索引值的两列的矩阵，具体看excl表格
-        #tolist函数则将他们变为列表，除去索引值
+
+    def twoline_graph(self):  # 半年线图
+        # self.stock1['close']取出来的值都是带有索引值的两列的矩阵，具体看excl表格
+        # tolist函数则将他们变为列表，除去索引值
 
         close = self.stock1['close'].tolist()
         strdate = self.stock1['trade_date'].tolist()
-        date=[]
+        date = []
         for i in strdate:
-            X = datetime.strptime(i, '%Y%m%d')           
-            date.append(X)         
+            X = datetime.strptime(i, '%Y%m%d')
+            date.append(X)
 
-        # 画图语句：go.Scatter
         trace = [go.Scatter(
-                            x=date,
-                            y=close
-                            )]
-        #print (trace)  
-        #[Scatter({'x': [2019-09-30 00:00:00, 2019-09-27 00:00:00.....
-                
+            x=date,
+            y=close
+        )]
         layout = dict(
-              title=self.name1,
-              xaxis=dict(title='日期'),
-              yaxis=dict(title='价格')
-              )
-        #print (layout)
-        #{'title': 'ETF50', 'xaxis': {'title': '日期'}, 'yaxis': {'title': '价格'}}
-        fig = dict(data=trace, layout=layout)  
-        #print (fig)
+            title=self.name1,
+            xaxis=dict(title='日期'),
+            yaxis=dict(title='价格')
+        )
+
+        fig = dict(data=trace, layout=layout)
         div = pyplt(fig, output_type='div', include_plotlyjs=False, auto_open=False, show_link=False)
-        #print (div)
+        # print (div)
         return div
 
     def high_low(self):
@@ -111,11 +117,8 @@ class Chart_Plot:
                 y=low
             )
         ]
-        # print (trace)
-        # [Scatter({'x': [2019-09-30 00:00:00, 2019-09-27 00:00:00.....
-
         layout = dict(
-            title=self.stock1['ts_code'].values[0]+":"+self.name1,
+            title=self.stock1['ts_code'].values[0] + ":" + self.name1,
             xaxis=dict(title='日期'),
             yaxis=dict(title='价格')
         )
@@ -125,7 +128,47 @@ class Chart_Plot:
         # print (fig)
         div = pyplt(fig, output_type='div', include_plotlyjs=False, auto_open=False, show_link=False)
         return div
-    
+
+    def plot_pes(self):
+        try:
+            print(self.stock1_code)
+            print("start:{} end:{}".format(self.start_date, self.end_date))
+            # import pdb
+            # pdb.set_trace()
+            if self.index_type == 'LYR':
+                df = pro.daily(ts_code=self.stock1_code, start_date=self.start_date, end_date=self.end_date,
+                                     fields='trade_date,pe')
+            else:
+                df = pro.daily(ts_code=self.stock1_code, start_date=self.start_date, end_date=self.end_date,
+                                     fields='trade_date,pe_ttm')
+        except Exception as e:
+            import traceback
+            traceback.print_exc()
+            return "无法获取该股票市盈率"
+        self.stock1 = df
+        print(self.stock1)
+        # 处理数据
+        dates, pes = [], []
+        for idx, row in df.iterrows():
+            trade_date, pe = row[0], row[1]
+            current_date = datetime.datetime.strptime(trade_date, '%Y%m%d')
+            dates.append(current_date)
+            pes.append(pe)
+
+        # 画图语句：go.Scatter
+        trace = [go.Scatter(
+            x=dates,
+            y=pes
+        )]
+        layout = dict(
+            title=self.name1,
+            xaxis=dict(title='日期'),
+            yaxis=dict(title='价格')
+        )
+        fig = dict(data=trace, layout=layout)
+        div = pyplt(fig, output_type='div', include_plotlyjs=False, auto_open=False, show_link=False)
+        return div
+
 # df=pd.read_csv("stock_code.csv")
 # print(df.head())
 # print(df.columns)
--- etf50-main.py
+++ etf50-main.py
@@ -4,17 +4,18 @@
 # This is main enterance of project.
 # visit http://127.0.0.1:5000/, you can see the graphic
 
+import datetime
+
+from chart_plot import Chart_Plot
 from flask import Flask
 from flask import render_template
 from flask import request
-from flask import url_for
-from flask import redirect
-from chart_plot import Chart_Plot
-import datetime
+
 app = Flask(__name__)
 
-chart = Chart_Plot("20200101","20201101")
+chart = Chart_Plot(start_date="20200101", end_date="20201101")
 context = {}  # 字典
+
 
 # 获取前1天或N天的日期，beforeOfDay=1：前1天；beforeOfDay=N：前N天
 def getdate(beforeOfDay):
@@ -24,51 +25,59 @@ def getdate(beforeOfDay):
     # 获取想要的日期的时间
     re_date = (today + offset).strftime('%Y-%m-%d')
     return re_date
+
+
 def get_value():
-    graph_type=request.form.get("graph_type")
-    stock_id=request.form.get("stock_id")
-    stock_id2=request.form.get("stock_id2")
-    start_date=request.form.get("start_date")
-    end_date=request.form.get("end_date")
+    graph_type = request.form.get("graph_type")
+    stock_id = request.form.get("stock_id")
+    stock_id2 = request.form.get("stock_id2")
+    start_date = request.form.get("start_date")
+    end_date = request.form.get("end_date")
     # 判断用户选择的日期，若没有选择开始日期，默认绘制最近一年的数据
     start_date = getdate(365) if start_date == "" else start_date
     end_date = getdate(1) if end_date == "" else end_date
-    #判断用户输入的股票id，如果为空，默认为000001
-    stock_id="000001" if stock_id=="" else stock_id
-    stock_id2="000002" if stock_id2=="" else stock_id2
+    # 判断用户输入的股票id，如果为空，默认为000001
+    stock_id = stock_id if stock_id else "000001"
+    stock_id2 = stock_id2 if stock_id2 else "000002"
 
-    period=request.form.get("period")
+    period = request.form.get("period")
+    index_type = request.form.get("type")
     return {
-        "graph_type":graph_type,
-        "stock_id":stock_id,
-        "stock_id2":stock_id2,
-        "start_date":start_date,
-        "end_date":end_date,
-        "period":period
+        "graph_type": graph_type,
+        "stock_id": stock_id,
+        "stock_id2": stock_id2,
+        "start_date": start_date,
+        "end_date": end_date,
+        "period": period,
+        "index_type": index_type
     }
 
 
-@app.route('/',methods=["POST","GET"])
+@app.route('/', methods=["POST", "GET"])
 def index():
     context['graph'] = chart.twoline_graph()
     return render_template("chars.html", title='Home', context=context)
 
-@app.route('/search',methods=["POST","GET"])
+
+@app.route('/search', methods=["POST", "GET"])
 def search():
-    #获取用户输入值
+    # 获取用户输入值
     values = get_value()
     print(values)
-    graph_type=values.get("graph_type")
-    #创建绘图对象
-    chart = Chart_Plot(start_date=values.get("start_date"), end_date=values.get("end_date"),stock1=values.get("stock_id"), stock2=values.get("stock_id2"))
-    if graph_type=="半年线图":
+    graph_type = values.get("graph_type")
+    # 创建绘图对象
+    chart = Chart_Plot(**values)
+    if graph_type == "半年线图":
         context['graph'] = chart.twoline_graph()
-    elif graph_type=="k线图":
+    elif graph_type == "k线图":
         context['graph'] = chart.candle_stick(values.get("period"))
-    elif graph_type=="高低点":
+    elif graph_type == "高低点":
         context['graph'] = chart.high_low()
+    elif graph_type == "市盈率":
+        context['graph'] = chart.plot_pes()
 
     return render_template("chars.html", title='Home', context=context)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
